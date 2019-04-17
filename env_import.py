a9e25d79b3905712e4b47b7e4ea212a6b9fe42ca
einom.ng@gmail.com <starBB002347>
2019-04-17 19:34:47 +0800

    111

---
+++ env_import.py
@@ -0,0 +1,20 @@
+import serial, keyboard, sys
+import win32api, win32gui, winsound
+import time, random, pyautogui
+import logging, json
+import cv2, os
+from datetime import datetime
+from tkinter import *
+from PIL import Image, ImageFilter, ImageChops
+import pytesseract
+import numpy as np
+import PIL.ImageOps
+import pyperclip
+import pyttsx3
+from win32api import GetKeyState
+from win32con import VK_CAPITAL
+import winshell, psutil
+import pandas as pd
+import shutil
+from colorama import init, Fore, Back, Style
+import matplotlib.pyplot as plt
